4c7831770880a0317da460b778de05494bc95ab7
kayrat <kairatomurbek2@gmail.com>
2017-12-01 00:55:59 +0600

    Вывел детальную страницу письма

--- web-application/webapp/urls.py
+++ web-application/webapp/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-
-from webapp.views import HomeView
+from webapp.views import HomeView, DetailLetterView
 
 urlpatterns = [
-    url(r'^$', HomeView.as_view(), name='home')
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^letters/(?P<pk>\d+)/$', DetailLetterView.as_view(), name="letter_detail"),
 ]
--- web-application/webapp/views.py
+++ web-application/webapp/views.py
@@ -1,7 +1,19 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
+from django.views.generic import TemplateView
+
 from webapp.models import Letter
 
 
 class HomeView(ListView):
     template_name = 'home.html'
     model = Letter
+
+
+class DetailLetterView(TemplateView):
+    template_name = 'detail-letter.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(DetailLetterView, self).get_context_data(**kwargs)
+        context['letter'] = get_object_or_404(Letter, pk=self.kwargs['pk'])
+        return context
