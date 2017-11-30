646d0fd05fa1ec2838f62020eb52cc08b63da920
kayrat <kairatomurbek2@gmail.com>
2017-12-01 00:03:11 +0600

    Создал модуль webapp, создал сущность "letter" and "Organization" и вывел в админку

--- web-application/webapp/admin.py
+++ web-application/webapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Letter, Organization
+
+admin.site.register(Letter)
+admin.site.register(Organization)
--- web-application/webapp/models.py
+++ web-application/webapp/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+ACTIVE = 'AC'
+MODERATION = 'MD'
+DECLINE = 'DC'
+
+STATUS_CHOICES = (
+    (ACTIVE, _('Свободен')),
+    (MODERATION, _('На расмотрение')),
+    (DECLINE, _('Забронирован')),
+)
+
+
+class Organization(models.Model):
+    name = models.CharField(max_length=150, verbose_name=_('Название организации'))
+    street_address = models.CharField(max_length=350, verbose_name=_('Адрес организации'))
+    phone = models.IntegerField(max_length=20, verbose_name=_('Номер телеофна организации'))
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = _("Организация")
+        verbose_name_plural = _("Организации")
+
+
+class Letter(models.Model):
+    image = models.ImageField(verbose_name=_("Письмо"), upload_to='images')
+    status = models.CharField(choices=STATUS_CHOICES, max_length=2, default='AC', verbose_name=_('Статус'))
+    organization = models.ForeignKey(Organization, related_name='letters', blank=True, null=True)
+
+    class Meta:
+        verbose_name = _("Письмо")
+        verbose_name_plural = _("Письма")
