ddfe532188f0e78cdff18f7045770211aeb007bb
kayrat <kairatomurbek2@gmail.com>
2017-12-01 02:14:45 +0600

    Переделал админку вывод дед морозов

--- web-application/webapp/admin.py
+++ web-application/webapp/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 
 from webapp.models import Letter, Organization, SantaClaus
 
+
+class SantaClausAdmin(admin.ModelAdmin):
+    list_per_page = 50
+    search_fields = ['name', 'phone']
+    list_display = ['name', 'phone']
+    readonly_fields = ('name', 'phone')
+
+
 admin.site.register(Letter)
 admin.site.register(Organization)
-admin.site.register(SantaClaus)
+admin.site.register(SantaClaus, SantaClausAdmin)
