f0e277947e7f70ec43de4ea3a7c1d70f2a3346e4
kayrat <kairatomurbek2@gmail.com>
2017-12-01 22:01:02 +0600

    django-jet add site

--- web-application/main/urls.py
+++ web-application/main/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.conf.urls.static import static
 
 urlpatterns = [
+                  url(r'^jet/', include('jet.urls', 'jet')),
+                  url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
                   url(r'^admin/', admin.site.urls),
                   url(r'^i18n/', include('django.conf.urls.i18n')),
                   url(r'^', include('webapp.urls')),
