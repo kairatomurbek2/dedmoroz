f92fa63ee8fa98b2c10a0a32faf303bd56fc2179
kayrat <kairatomurbek2@gmail.com>
2017-12-05 15:35:42 +0600

    add robots.txt

--- web-application/main/urls.py
+++ web-application/main/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
                   url(r'^jet/', include('jet.urls', 'jet')),
@@ -9,4 +10,8 @@ urlpatterns = [
                   url(r'^admin/', admin.site.urls),
                   url(r'^i18n/', include('django.conf.urls.i18n')),
                   url(r'^', include('webapp.urls')),
+                  url(r'^robots.txt$', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+                  url(r'^google8d646686634b60ab.html$',
+                      TemplateView.as_view(template_name="google3180cd19f46996bd.html",
+                                           content_type="text/html")),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
