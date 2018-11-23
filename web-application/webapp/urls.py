952614b2c54dcc609435e2cdb584b6b205071a1e
kairat <kairatomurbek2@gmail.com>
2018-11-23 19:25:36 +0600

    Change url

--- web-application/webapp/urls.py
+++ web-application/webapp/urls.py
@@ -4,5 +4,5 @@ from webapp.views import HomeView, DetailLetterView, CreateSantaClausView
 urlpatterns = [
     path(r'', HomeView.as_view(), name='home'),
     path('letters/<int:id>/', DetailLetterView.as_view(), name="letter_detail"),
-    path('^letters/<int:id>/create/$', CreateSantaClausView.as_view(), name="create_santa"),
+    path('letters/<int:id>/create/$', CreateSantaClausView.as_view(), name="create_santa"),
 ]
